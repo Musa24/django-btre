a9e589416b93c6b25db0d75e712c5d7144ccaf86
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2021-03-04 11:41:45 +0300

    pagination,order,filter

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
 from django.template import loader
 from .models import Listing
 
 
+
 def index(request):
-   listings = Listing.objects.all()
+#    listings = Listing.objects.all()
+   listings = Listing.objects.order_by("-list_date").filter(is_published=True)
+   paginator = Paginator(listings,3)  # show X list per page
+   page_number = request.GET.get("page")
+   page_Obj = paginator.get_page(page_number)
    for listing in listings:
        print(listing.list_date)
    context = {
-       "listings":listings
+       "listings":page_Obj
    }
    return render(request,"listings/listings.html",context)
 
@@ -19,3 +25,5 @@ def listing(request,listing_id):
 
 def search(request):
     return render(request,"listings/search.html")
+
+
