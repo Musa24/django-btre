40c84cf03c32b4a644f9a2077624c0bf837ef36e
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2021-03-04 13:24:33 +0300

    Home,about page

--- pages/views.py
+++ pages/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 def index(request):
-    print(request.path == "/")
-    return render(request,"pages/index.html")
-
+    listings = Listing.objects.order_by("-list_date").filter(is_published = True)[:3]
+    
+    context = {
+        "listings":listings
+    }
+    return render(request,"pages/index.html",context)
 
 def about(request):
-    # print("-------------------")
-    # print('about1' in request.path)
-    # print("-------------------")
+    realtors = Realtor.objects.order_by("-hire_date")
+    mvp_realtor = Realtor.objects.filter(is_mvp = True)[0]
+
+    context = {
+        "realtors":realtors,
+        "mvp_realtor":mvp_realtor
+
+    }
     template = loader.get_template("pages/about.html") #Loading template
-    return HttpResponse(template.render())       # rendering the template in HttpResponse  
+    return HttpResponse(template.render(context))       # rendering the template in HttpResponse  
