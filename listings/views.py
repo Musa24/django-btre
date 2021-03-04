b33905d6d37bc09192a2bdcfdfa691bdde80c537
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2021-03-04 09:10:05 +0300

    Display a template woth data from DB

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Listing
+
 
 def index(request):
-   return render(request,"listings/listings.html")
+   listings = Listing.objects.all()
+   for listing in listings:
+       print(listing.list_date)
+   context = {
+       "listings":listings
+   }
+   return render(request,"listings/listings.html",context)
 
-def listing(request):
+def listing(request,listing_id):
     template = loader.get_template("listings/listing.html")
     return HttpResponse(template.render())
 
